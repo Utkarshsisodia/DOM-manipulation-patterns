22c8785d622b3e96e11924fff21f12449b5585ef
Utkarshsisodia <usisodia67@gmail.com>
2025-06-16 15:47:36 +0530

    Add CSS

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 // Selector Caching
 const form = document.getElementById("form");
-const formInput = document.getElementById("form-input");
-const taskList = document.getElementById("task-list");
+const input = document.getElementById("form-input");
+const list = document.getElementById('task-list');
 
 // Batch DOM updates using fragment
-function add(text) {
+function addTask(text){
     let fragment = document.createDocumentFragment();
     let li = document.createElement("li");
     li.textContent = text;
-    li.className = 'task';
-    li.innerHTML += "<button class='delete'>Delete</button>"
+    li.className = "task";
+    li.innerHTML += "<button class='delete'>delete</button>";
     fragment.appendChild(li);
-    taskList.appendChild(fragment);
+    list.appendChild(fragment);
 }
 
 // Event Delegation
-taskList.addEventListener("click", (e)=>{
-    if(e.target.classList.contains("delete")){
-        e.target.parentElement.remove();
-    }else if(e.target.classList.contains("task")){
+list.addEventListener("click", (e)=>{
+    if(e.target.classList.contains("task")){
         e.target.classList.toggle("completed");
+    }else if(e.target.classList.contains("delete")){
+        e.target.parentElement.remove();
     }
 })
 
 // Form submit handler
-form.addEventListener("submit",(e)=>{
+form.addEventListener("submit", (e)=>{
     e.preventDefault();
-    add(formInput.value);
-    formInput.value = '';
+    addTask(input.value);
+    input.value = '';
 })
